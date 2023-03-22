67bafda239fe505ce7db242f2d72cac095c44a2f
Hloya Fox <hloyatrash@gmail.com>
2023-03-22 20:25:17 +0400

    fix server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
-const config = require('./config');
+const config = require('./config/config');
 
 const app = express();
 app.use(express.json());
