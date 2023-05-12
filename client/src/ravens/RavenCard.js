3c58b00ebb228ec5be42d67a890c8daee49aecb5
Hloya Fox <hloyatrash@gmail.com>
2023-05-12 21:51:10 +0400

    добавление картинок для воронов

--- client/src/ravens/RavenCard.js
+++ client/src/ravens/RavenCard.js
@@ -3,13 +3,20 @@ import { withRouter } from '../withRouter';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import White from '../image/White';
+import Black from '../image/Black';
 
 // карточка ворона
 
 class RavenCard extends React.Component {
   sendMessage = () => {
     this.props.navigate(`/location/send/${this.props.ravenId}`, {
-      state: { ravenId: this.props.ravenId, locationId: this.props.locationId },
+      state: {
+        ravenId: this.props.ravenId,
+        locationId: this.props.locationId,
+        weight: this.props.weight,
+        isWhite: this.props.isWhite,
+      },
     });
   };
 
@@ -19,6 +26,9 @@ class RavenCard extends React.Component {
         <Card border="dark">
           <Card.Body>
             <Card.Title>{this.props.name}</Card.Title>
+
+            {this.props.isWhite === 1 ? <White /> : <Black />}
+
             <Button variant="outline-dark" onClick={this.sendMessage}>
               Отправить ворона
             </Button>
